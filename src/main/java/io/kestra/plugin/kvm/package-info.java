4f1623ce8c60cbb314357b7e0b1badb9ab09b8da
Vamsi <vamsi.172323@gmail.com>
2025-12-26 21:28:38 +0530

    kvm plugin:updating svg and package-info.java

---
+++ src/main/java/io/kestra/plugin/kvm/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This subgroup of plugins contains tasks to manage virtual machines on remote KVM/libvirt hosts.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
+)
+package io.kestra.plugin.kvm;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
