b0ee0fa2f43edf861116bd6e178780c7d8e55a17
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 12:22:21 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/kvm/package-info.java
+++ src/main/java/io/kestra/plugin/kvm/package-info.java
@@ -1,5 +1,5 @@
 @PluginSubGroup(description = "This subgroup of plugins contains tasks to "
-        + "manage virtual machines on remote KVM/libvirt hosts.", categories = { PluginSubGroup.PluginCategory.TOOL })
+        + "manage virtual machines on remote KVM/libvirt hosts.", categories = PluginSubGroup.PluginCategory.INFRASTRUCTURE)
 package io.kestra.plugin.kvm;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
